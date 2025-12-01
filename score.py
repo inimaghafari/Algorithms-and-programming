ff98c2e568807ff210928cb859bf24d13650421f
Nima Ghafari <nima.ghafari5835@gmail.com>
2025-12-02 00:13:31 +0330

    score.py

--- score.py
+++ score.py
@@ -1,10 +1,11 @@
 score = int(input("Give me your score: "))
 
-if score > 90:
+if score >= 90:
     print ("A")
-elif 80 <= score <= 90:
+elif 80 <= score < 90:
     print("B")
 elif 70 <= score < 80:
     print("C")
 else:
     print("D")
+
